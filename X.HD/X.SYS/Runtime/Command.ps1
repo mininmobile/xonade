31e425f092c27ff5627fe0d48ea5ea9c8f7ca055
MininMobile <notminin@pm.me>
2018-10-19 23:32:16 +0100

    add command parser

--- X.HD/X.SYS/Runtime/Command.ps1
+++ X.HD/X.SYS/Runtime/Command.ps1
@@ -1,2 +1,25 @@
 # copyleft xonade, licensed under MIT
 # command interpreter
+
+param (
+	[string] $command
+)
+
+# remove undentation
+$c = $command -replace "^[ |\t]+", ""
+
+# labels
+$c = $c -replace "^:", "label "
+
+# empty command
+if ($c.Length -eq 0) { $c = "null" }
+
+# lowercase
+if ($c.Contains("`"")) {
+	$strings = $c.Split("`"")
+	for ($i = 0; $i -lt $strings.Count; $i++) { if ((($i) % 2) -eq 0) { $strings[$i] = $strings[$i].ToLower() } }
+	$c = ([string]::Join("`"", $strings))
+}
+
+# return formatted command
+$c
